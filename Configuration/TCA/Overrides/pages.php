8e9e05f5ec67f2033f2ddbfa29013974615a2049
Steffen Kroggel <developer@steffenkroggel.de>
2021-11-16 21:14:11 +0100

    [FIX] Adding checkbox isImportSub to TCA

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -7,25 +7,33 @@ $tempColumnsPages = array(
 
     'tx_rkwpdf2content_is_import' => array (
         'exclude' => 0,
-        'displayCond' => 'FIELD:tx_rkwpdf2content_is_import_sub:=:0',
+        // 'displayCond' => 'FIELD:tx_rkwpdf2content_is_import_sub:=:0',
         'label' => 'LLL:EXT:rkw_pdf2content/Resources/Private/Language/locallang_db.xlf:tx_rkwpdf2content_domain_model_pages.tx_rkwpdf2content_is_import',
         'config' => array (
             'type' => 'check',
-            'default' => 0,
+            'default' => 1,
             'items' => array(
                 '1' => array(
                     '0' => 'LLL:EXT:rkw_pdf2content/Resources/Private/Language/locallang_db.xlf:tx_rkwpdf2content_domain_model_pages.tx_rkwpdf2content_is_import.I.enable'
                 )
             )
-        )
+        ),
+        'onChange' => 'reload'
     ),
     'tx_rkwpdf2content_is_import_sub' => array (
-        'exclude' => 0,
-        'label' => 'LLL:EXT:rkw_pdf2content/Resources/Private/Language/locallang_db.xlf:tx_rkwpdf2content_domain_model_pages.tx_rkwpdf2content_is_import_sub',
+        'exclude' => 1,
+        'displayCond' => 'FIELD:tx_rkwpdf2content_is_import:=:1',
+        // 'label' => 'LLL:EXT:rkw_pdf2content/Resources/Private/Language/locallang_db.xlf:tx_rkwpdf2content_domain_model_pages.tx_rkwpdf2content_is_import_sub',
         'config' => array (
             'type' => 'check',
             'default' => 0,
-        )
+            'items' => array(
+                '1' => array(
+                    '0' => 'LLL:EXT:rkw_pdf2content/Resources/Private/Language/locallang_db.xlf:tx_rkwpdf2content_domain_model_pages.tx_rkwpdf2content_is_import_sub.I.enable'
+                )
+            )
+        ),
+        'onChange' => 'reload'
     ),
 
     //***********
@@ -37,7 +45,7 @@ $tempColumnsPages = array(
         'label' => 'LLL:EXT:rkw_pdf2content/Resources/Private/Language/locallang_db.xlf:tx_bmpdf2content_domain_model_pages.tx_bmpdf2content_is_import' . ' (deprecated)',
         'config' => array (
             'type' => 'check',
-            'default' => 0,
+            'default' => 1,
             'items' => array(
                 '1' => array(
                     '0' => 'LLL:EXT:bm_pdf2content/Resources/Private/Language/locallang_db.xlf:tx_bmpdf2content_domain_model_pages.tx_bmpdf2content_is_import.I.enable'
@@ -46,7 +54,7 @@ $tempColumnsPages = array(
         )
     ),
     'tx_bmpdf2content_is_import_sub' => array (
-        'exclude' => 0,
+        'exclude' => 1,
         'label' => 'LLL:EXT:rkw_pdf2content/Resources/Private/Language/locallang_db.xlf:tx_bmpdf2content_domain_model_pages.tx_bmpdf2content_is_import_sub' . ' (deprecated)',
         'config' => array (
             'type' => 'check',
@@ -60,4 +68,4 @@ $tempColumnsPages = array(
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('pages',$tempColumnsPages);
 
 // Add field to the existing palette
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addFieldsToPalette('pages', 'tx_rkwbasics_extended', 'tx_rkwpdf2content_is_import,');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addFieldsToPalette('pages', 'tx_rkwbasics_extended', 'tx_rkwpdf2content_is_import,tx_rkwpdf2content_is_import_sub');
